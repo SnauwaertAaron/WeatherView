6c29e426a51146ddb637b3048729faf9d7ec5a68
SnauwaertAaron <55881716+SnauwaertAaron@users.noreply.github.com>
2020-11-24 21:21:15 +0100

    Wind Direction Arrow fixed

--- script/app.js
+++ script/app.js
@@ -1,18 +1,21 @@
+// Opgehaalde waarden weergeven op pagina
 const showResult = (queryResponse) => {
     console.log(queryResponse);
 
-    htmlTemperature.innerText = `${queryResponse.main.temp}°C`;
-    htmlWindChill.innerText = `${queryResponse.main.feels_like}°C`;
+    htmlTemperature.innerText = `${Math.round(queryResponse.main.temp)}°C`;
+    htmlWindChill.innerText = `${Math.round(queryResponse.main.feels_like)}°C`;
     htmlHumidity.innerText = `${queryResponse.main.humidity}%`;
     htmlWindSpeed.innerText = `${queryResponse.wind.speed} km/h`;
-    htmlWinDirection.innerText = `${queryResponse.wind.deg}°`;
+    let rotateAngle = `${queryResponse.wind.deg}`;
+    console.log(rotateAngle);
+
+    arrow.setAttribute("transform", `rotate(${rotateAngle})`);
+
 };
 
 
+// API opvragen aan de hand van de ingegeven stad
 const getAPI = async (cityname) => {
-    // Eerst bouwen we onze url op
-    // Met de fetch API proberen we de data op te halen.
-    // Als dat gelukt is, gaan we naar onze showResult functie.
     const endpoint = `http://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=0d83dff417d181fcf3def092e000cdf6&units=metric&lang=nl&cnt=1`;
 
     const data = await fetch(endpoint)
@@ -22,13 +25,10 @@ const getAPI = async (cityname) => {
 };
 
 
-
-
+// Listener toevoegen aan de button
 const listenToSearchOnCity = function(){
-    let afgeschaftwaarde
     const button = document.querySelector('.js-Cityinput');
     button.addEventListener('click', function(){
-
         getAPI(document.getElementById('CityText').value);
     });
   };
@@ -36,13 +36,15 @@ const listenToSearchOnCity = function(){
 
 
 
+// DOMContentloaded aanmaken
 document.addEventListener('DOMContentLoaded', function () {
     htmlBody = document.querySelector('body');
     htmlTemperature = document.querySelector('.js-Temperature');
     htmlWindChill = document.querySelector('.js-WindChill');
     htmlHumidity = document.querySelector('.js-Humidity');
     htmlWindSpeed = document.querySelector('.js-WindSpeed');
-    htmlWinDirection = document.querySelector('.js-WindDirection');
+    arrow = document.querySelector('.js-WindDirection');
+
 
     listenToSearchOnCity();
 });
