736ab5ae9751c5f088620b5e3e1e6b667316f91d
SnauwaertAaron <55881716+SnauwaertAaron@users.noreply.github.com>
2020-11-24 19:43:48 +0100

    Thermometer fixed and JS static elements

---
+++ script/app.js
@@ -0,0 +1,34 @@
+const showResult = (queryResponse) => {
+    console.log(queryResponse);
+
+    htmlTemperature.innerText = `${queryResponse.main.temp}°C`;
+    htmlWindChill.innerText = `${queryResponse.main.feels_like}°C`;
+    htmlHumidity.innerText = `${queryResponse.main.humidity}%`;
+    htmlWindSpeed.innerText = `${queryResponse.wind.speed} km/h`;
+    htmlWinDirection.innerText = `${queryResponse.wind.deg}°`;
+    
+};
+
+const getAPI = async (cityname) => {
+    // Eerst bouwen we onze url op
+    // Met de fetch API proberen we de data op te halen.
+    // Als dat gelukt is, gaan we naar onze showResult functie.
+    const endpoint = `http://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=0d83dff417d181fcf3def092e000cdf6&units=metric&lang=nl&cnt=1`;
+
+    const data = await fetch(endpoint)
+        .then((r) => r.json())
+        .catch((err) => console.error('An error occured', err));
+    showResult(data);
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+    htmlBody = document.querySelector('body');
+    htmlTemperature = document.querySelector('.js-Temperature');
+    htmlWindChill = document.querySelector('.js-WindChill');
+    htmlHumidity = document.querySelector('.js-Humidity');
+    htmlWindSpeed = document.querySelector('.js-WindSpeed');
+    htmlWinDirection = document.querySelector('.js-WindDirection');
+
+    // 1 We will query the API with longitude and latitude.
+    getAPI("London");
+});
